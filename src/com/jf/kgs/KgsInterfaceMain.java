2c0e2f56ad208c11cac8c33994f39034b5ba07d7
alleton <jfalleton@gmail.com>
2018-10-13 16:29:52 +0200

    1 commit

---
+++ src/com/jf/kgs/KgsInterfaceMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jf.kgs;
+
+/**
+ * @author jf
+ *
+ */
+public class KgsInterfaceMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
